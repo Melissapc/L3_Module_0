bf75f4487216bd2e3351b405abbf4a04825e786b
100297 <league@WTS7.attlocal.net>
2017-08-23 18:27:17 -0700

    wednesday august 23, 2017
    level 3 stuff. code try to remember !! ha

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,7 +1,51 @@
 package IntroToArrayLists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+
+
+	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+	public static void main(String[] args) {
+		new GuestBook();
+	}
+
+	public GuestBook() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(600, 500);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		JButton addname = new JButton();
+
+		JButton viewname = new JButton();
+		panel.add(addname);
+		panel.add(viewname);
+
+		
+		
+		
+		
+		addname.setText("Add Name");
+		viewname.setText("View Name");
+		addname.addActionListener(this);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+
+	}
+
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
